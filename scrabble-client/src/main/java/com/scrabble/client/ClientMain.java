cbf43c3b2023164a18d9d730af4a5693fc47d577
Seyyed Shayan Daneshvar <daneshvarshayan@gmail.com>
2020-11-06 20:29:20 +0330

    Revert "Dev"

--- scrabble-client/src/main/java/com/scrabble/client/ClientMain.java
+++
@@ -1,9 +0,0 @@
-package com.scrabble.client;
-
-import com.scrabble.client.controller.MainController;
-
-public class ClientMain {
-    public static void main(String[] args) {
-        MainController.init(args);
-    }
-}
--- scrabble-client/src/main/java/com/scrabble/client/view/GameView.java
+++
@@ -1,7 +0,0 @@
-package com.scrabble.client.view;
-
-public interface GameView<T> {
-
-    void drawGameBoard(T t);
-
-}
